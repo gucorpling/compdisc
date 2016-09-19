1a1e52571cc5eeac274d82097900df409913f7e7
amir-zeldes <amir.zeldes@georgetown.edu>
2016-09-19 11:45:19 -0400

    Implemented WindowDiff   * Closes #2

--- scoring.py
+++ scoring.py
@@ -101,3 +101,29 @@ def find_boundaries(scores, smoothing_window=2, type='liberal'):
     depth_scores = depth_scoring(smoothed_scores)
     boundaries = boundarize(depth_scores, type)
     return tuple(boundaries)
+
+
+def window_diff(predicted_tiles, gold_tiles, window_size):
+    """
+    Function implementing WindowDiff scoring from Pevzner & Hearst (2002). Takes two equal length lists
+    of 1's and 0's and reports WindowDiff error penalty metric for selcted window_size (lower is better).
+
+    :param predicted_tiles: list of sentence breaks at which tiles are predicted to begin [1,0,0,1, ...]
+    :param gold_tiles: gold list of same length
+    :param window_size: integer, must be smaller than text length in tiles
+    :return: score as float, 0 =< score =< 1
+    """
+
+    errors = 0
+    if window_size > len(gold_tiles):
+        raise IndexError("Window size " + str(window_size) + " too large for text length " + str(len(gold_tiles)) + " tiles.\n")
+    if len(predicted_tiles) != len(gold_tiles):
+        raise IndexError("Gold tiles and predicted tiles must have same lengt. Found: " + str(len(gold_tiles)) + " gold tiles but " + str(len(predicted_tiles)) + " predicted tiles \n")
+
+    for index, tile in enumerate(gold_tiles[:len(gold_tiles)-window_size]):
+        gold_boundaries = sum(gold_tiles[index:index+window_size])
+        predicted_boundaries = sum(predicted_tiles[index:index+window_size])
+        if gold_boundaries != predicted_boundaries:
+            errors += 1
+
+    return (1/(len(gold_tiles)-float(window_size))) * errors
