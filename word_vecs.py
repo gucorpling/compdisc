0c31183cecd0fef1dfde1b96579662aef964cd33
james <jamesrmaguire@live.co.uk>
2016-09-28 11:31:12 -0400

    updated word_vecs and added a file of the predictions by vectors

--- word_vecs.py
+++ word_vecs.py
@@ -7,7 +7,7 @@ from tile_reader import TileReader
 
 options = {'vocab_tags': ["NOUN", "PROPN"],
            'block_length': 3,
-           'smoothing_window': 2,
+           'smoothing_window': None,  # int or None
            'smoothing_type': 'liberal',  # liberal or conservative
            'out_type': 0}  # 0 or 1
 
@@ -28,10 +28,9 @@ def _vectorize(block, options):
 def tile(filename, options=options):
     filename = os.getcwd() + "\\" + filename
     reader = TileReader()
-    reader.read(filename)
+    reader.read(filename, newline_tokenization=True)
     reader.set_vocab_tags(options['vocab_tags'])
     blocks = reader.get_blocks(options['block_length'])
-    print(len(reader.sentences))
 
     # placeholder for similarity scores
     similarity_scores = []
@@ -41,9 +40,10 @@ def tile(filename, options=options):
         vecB = _vectorize(blockB, options)
         similarity_scores.append(cosine(vecA, vecB))
 
-    bounds_1 = find_boundaries(similarity_scores, options['smoothing_window'], options['smoothing_type'])
-    bounds_2 = boundarize(depth_scoring(similarity_scores), options['smoothing_type'])
-    bounds = bounds_2
+    if options['smoothing_window'] is None:
+        bounds = boundarize(depth_scoring(similarity_scores), options['smoothing_type'])
+    else:
+        bounds = find_boundaries(similarity_scores, options['smoothing_window'], options['smoothing_type'])
 
     if options['out_type'] == 1:
         out_string = ''
@@ -55,24 +55,26 @@ def tile(filename, options=options):
             count += 1
         return out_string
     else:
-        out_list = [1]
+        out_list = []
         for i in xrange(len(reader.sentences)):
-            if i in bounds:
+            if i-(options['block_length']) in bounds:
                 out_list.append(1)
             else:
                 out_list.append(0)
+        out_list[0] = 1
         return out_list
 
 city = 'merida'
 
 segments = tile('data\\GUM_voyage_'+city+'_noheads.txt', options)
+print segments
 
 golds = {}
 with open(os.getcwd() + "\\data\\boundaries") as infile:
     for line in infile:
         line = line.split()
         golds[line[0]] = [int(x) for x in line[1:]]
-print 'predicted:   ', segments
-print 'gold:        ', golds[city]
+print 'predicted:   ', segments, len(segments)
+print 'gold:        ', golds[city], len(golds[city])
 
 print window_diff(segments, golds[city], 3)
